353ce24e6066e427d2ae6e61272b250bc515b8ac
Sergio Perez <sperez4mba@gmail.com>
2017-10-11 21:53:18 -0500

    Undid escaping

--- config/config-prod.py
+++ config/config-prod.py
@@ -1,5 +1,4 @@
 import os
-import urllib
 
 
 DEBUG = False
@@ -39,7 +38,7 @@ LOG_FILE_SIZE = int(env('LOG_FILE_SIZE', '10'))
 _MONGODB_AUTH_SOURCE = env('_MONGODB_AUTH_SOURCE', 'admin')
 _MONGODB_HOST = env('_MONGODB_HOST', '127.0.0.1')
 _MONGODB_USER = env('_MONGODB_USER', 'mongouser')
-_MONGODB_PASSWORD = urllib.quote(env('_MONGODB_PASSWORD', 'password'))
+_MONGODB_PASSWORD = env('_MONGODB_PASSWORD', 'password')
 _MONGODB_NAME = env('_MONGODB_NAME', 'mongodb')
 _MONGODB_CONN_CHAIN = "mongodb://{}:{}@{}/{}?authSource={}".format(
     _MONGODB_USER,
